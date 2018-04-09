c707a7e906c999d924a113ec666ac400bc1f11c7
Adrian Czech <czech400m@gmail.com>
2018-04-09 15:18:57 +0200

    builder && hire preferences

--- src/main/java/czechadrian/company/exceptions/CanHireException.java
+++ src/main/java/czechadrian/company/exceptions/CanHireException.java
@@ -2,6 +2,6 @@ package czechadrian.company.exceptions;
 
 public class CanHireException extends RuntimeException {
     public CanHireException() {
-        System.out.println("cant hire the employee");//super("cant hire the employee");
+        super("cant hire the employee");
     }
 }
